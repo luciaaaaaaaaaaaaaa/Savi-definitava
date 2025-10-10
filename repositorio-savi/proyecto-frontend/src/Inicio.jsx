9b1a2c0b6d26f9f9efb933ebd5f1d5b99ac37ec1
Lucia Ferreira <lucia.ferreira@anima.edu.uy>
2025-10-10 11:20:52 -0300

    Cambio Frontend-Leila

--- repositorio-savi/proyecto-frontend/src/Inicio.jsx
+++ repositorio-savi/proyecto-frontend/src/Inicio.jsx
@@ -39,7 +39,7 @@ const Inicio = ({ onGoRegistro, onGoInicioUsuario }) => {
           <div className="opcion-card">
             <img src="https://i.imgur.com/ZifMmLa.jpeg" alt="Opción 1" />
             <div className="opcion-card-overlay">
-              <p>Domino´s pizza</p>
+              <p>Kinko</p>
               <p>Poctitos</p>
               <p>Rampa</p>
             </div>
@@ -47,17 +47,17 @@ const Inicio = ({ onGoRegistro, onGoInicioUsuario }) => {
           <div className="opcion-card">
             <img src="https://i.imgur.com/ZifMmLa.jpeg" alt="Opción 2" />
             <div className="opcion-card-overlay">
-              <p>Domino´s pizza</p>
-              <p>Poctitos</p>
-              <p>Rampa</p>
+              <p>EstrellaFresca</p>
+              <p>Centro</p>
+              <p>Baño adaptado</p>
             </div>
           </div>
           <div className="opcion-card">
             <img src="https://i.imgur.com/ZifMmLa.jpeg" alt="Opción 3" />
             <div className="opcion-card-overlay">
-              <p>Domino´s pizza</p>
-              <p>Poctitos</p>
-              <p>Rampa</p>
+              <p>Geant</p>
+              <p>Carrasco</p>
+              <p>Caja accesible</p>
             </div>
           </div>
         </div>
@@ -71,29 +71,30 @@ const Inicio = ({ onGoRegistro, onGoInicioUsuario }) => {
       <section className="quienes-somos">
         <div className="texto">
           <h2>¿Quiénes somos?</h2>
-            <p> Nuestro equipo SAVI está conformado por 9 integrantes </p>
-          <div className="faq">
-            <div onClick={() => toggle(1)} className="faq-item">
-              <h3>¿Quiénes somos?</h3>
-              {open === 1 && <p>S.A.V.I. es un proyecto creado en Montevideo para mejorar cómo las grandes
-                 empresas y cadenas gestionan la accesibilidad de sus espacios. Inspirado en N.A.V.I., busca eliminar barreras físicas, sensoriales y comunicacionales para personas con discapacidad.</p>}
+          <p> Nuestro equipo SAVI está conformado por 9 integrantes comprometidos con la accesibilidad y la creación de entornos más inclusivos.</p>
+          <div className="quienes-grid">
+            <div className="quienes-card">
+              <h3>Misión</h3>
+              <p>Nos encargamos de facilitar el paso a información sobre locales accesibles en Montevideo, para que las personas en situación de disapacidad puedan elegir y disfrutar con libertad y confianza a través de una herramienta digital.</p>
             </div>
-            <div onClick={() => toggle(2)} className="faq-item">
-              <h3>¿Qué hacemos?</h3>
-              {open === 2 && <p>El proyecto busca establecer un estándar corporativo de accesibilidad en grandes empresas y cadenas de Montevideo. Con S.A.V.I., las compañías podrán registrar, verificar y comunicar la accesibilidad de sus locales, evitando la frustración de las personas con discapacidad al encontrar espacios no adaptados.</p>}
+            <div className="quienes-card">
+              <h3>Visión</h3>
+              <p>Buscamos convertirnos en la plataforma líder en inclusión digital en Montevideo y Uruguay, expandiéndose a otros rubros y promoviendo una sociedad más accesible, justa y participativa.</p>
             </div>
-            <div onClick={() => toggle(3)} className="faq-item">
-              <h3>¿Cómo contactarnos?</h3>
-              {open === 3 && <p>Puedes escribirnos a nuestro mail: savi@gmail.com.uy o a través de nuestras redes sociales.</p>}
+            <div className="quienes-card">
+              <h3>Valores</h3>
+              <p><strong>Inclusión</strong>: participación para todos. <strong>Accesibilidad</strong>: eliminación de barreras. <strong>Innovación</strong>: tecnología para mejorar vidas. <strong>Responsabilidad social</strong>: compromiso empresarial con la accesibilidad. <strong>Transparencia</strong>: información clara y confiable.</p>
             </div>
-            <div onClick={() => toggle(4)} className="faq-item">
-              <h3>¿Cómo ser colaborador?</h3>
-              {open === 4 && <p>Para colaborar puedes ponerte en contacto con nosotros a través de nuestro mail o redes sociales.</p>}
+            <div className="quienes-card">
+              <h3>Colaboradores</h3>
+              <p>Para colaborar puedes ponerte en contacto con nosotros a través de nuestro mail o redes sociales.</p>
             </div>
           </div>
-        </div>
-        <div className="imagen">
-          <img src="https://i.imgur.com/1o6HrcP.jpeg" alt="Accesibilidad" />
+          <div className="contacto-centrado">
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSedbA9MW9OLE-KpcYjyagUL7r2RMYh987UuhBZWXo6wNRkV7A/viewform">
+            <button className="contacto-btn">Contactanos</button>
+            </a>
+          </div>
         </div>
       </section>
 
